309fa760a1dbd0764c32dae62e7f4f402a3fa379
ethanrichards <ethan.richards@indwes.edu>
2015-12-09 03:35:39 +0000

    Finished

--- EuclidGCD.py
+++ EuclidGCD.py
@@ -1,4 +1,15 @@
+
 def gCd(x, y):
-    if ???# base case here
-        return ???
-    return gCd(?????)
+    print("X = ",x,"Y = ",y)
+    if y==0:
+        return x
+    else:    
+        return gCd(y,x%y)
+
+def main():
+    x = int(input("Input a number:"))
+    y = int(input("Input a number:"))
+    print("GCD of", x, ",",y,"=",gCd(x,y))
+    
+if __name__ =="__main__":
+    main()
